f14597bb5296b0b92a54e200a71f860eea901bfd
Mushet F <64835634+Mushet-F@users.noreply.github.com>
2021-03-18 15:21:34 +0000

    Add project files.

---
+++ Computershare/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Computershare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string extractData = "Y";
+
+            while (extractData == "Y")
+            {
+                try
+                {
+                    Console.WriteLine("Please provide the directory path to data sample: ");
+                    string filePath = Console.ReadLine();
+                    string rawData = File.ReadAllText(filePath);
+
+                    double[] pricesDataSet = rawData.Split(',').Select(n => Convert.ToDouble(n)).ToArray();
+
+                    Stock month = new Stock(pricesDataSet);
+
+                    Console.WriteLine(month.getBestTrade(month.Prices));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("Would you like to process another data sample? Please respond with (Y/N)");
+                extractData = Console.ReadLine();
+
+                while((extractData != "Y") && (extractData != "N"))
+                {
+                    Console.WriteLine("Please respond with (Y/N)");
+                    extractData = Console.ReadLine();
+                }  
+
+            }
+
+        }
+    }
+}
---
+++ Computershare/Stock.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computershare
+{
+    class Stock
+    {
+
+        public Stock(double[] prices)
+        {
+            this.Prices = prices;
+        }
+
+        public double[] Prices
+        {
+            get; set;
+        }
+
+        public string getBestTrade(double[] Prices)
+        {
+
+            double maxProfit = Prices[1] - Prices[0];
+            double minPrice = Prices[0];
+
+            double buyPrice = Prices[0];
+            int buyDate = 1;
+            int potentialBuyDate = 0;
+
+            double sellPrice = Prices[0];
+            int sellDate = 1;
+
+            for (int i = 1; i < Prices.Length; i++)
+            {
+
+                double currentProfit = Prices[i] - minPrice;
+
+                minPrice = Math.Min(minPrice, Prices[i]);
+                maxProfit = Math.Max(maxProfit, currentProfit);
+
+                if (minPrice == Prices[i])
+                {
+                    potentialBuyDate = i + 1;
+                }
+
+                if (Prices[i] == minPrice + maxProfit)
+                {
+                    sellDate = i + 1;
+                    sellPrice = Prices[i];
+                }
+
+                if (sellDate > potentialBuyDate && potentialBuyDate != 0)
+                {
+                    buyDate = potentialBuyDate;
+                    buyPrice = Prices[buyDate - 1];
+                    potentialBuyDate = 0;
+                }
+
+
+            }
+
+            string output = $"{buyDate}({buyPrice}){sellDate}({sellPrice})";
+
+            return output;
+
+        }
+
+    }
+
+}
